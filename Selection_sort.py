4ac93bfe45bd5fb851ab8dc3eb254e2b8694007a
Saksham Sharma <saksham8740@outlook.com>
2026-07-24 19:42:38 +0530

    selection sort

---
+++ Selection_sort.py
@@ -0,0 +1,21 @@
+#This is a selection sort code
+#what do we select
+#we select minimum element
+#Place it at beginning of the array
+class Solution:
+    def selectionSort(self, nums):
+        l=len(nums)
+        for i in range(l-1):
+            min_e=nums[i]
+            idx=i
+            for j in range(i+1,l):
+                if nums[j]<min_e:
+                    min_e=nums[j]
+                    idx=j
+                    print('min_e: ',min_e,' idx: ',idx,' in iteration: ',i)
+                    
+            temp=nums[i]
+            nums[i]=nums[idx]
+            nums[idx]=temp
+            print('after reshuffle: ',nums)
+        return nums
